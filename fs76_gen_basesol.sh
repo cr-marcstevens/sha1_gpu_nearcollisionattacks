0de004464d0c62b6a87ea7f0b161c4f86d021efd
cr-marcstevens <github@marc-stevens.nl>
2017-11-22 19:54:14 +0100

    Improved script.

--- fs76_gen_basesol.sh
+++ fs76_gen_basesol.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+export BINDIR=$(dirname $0)/bin
+export BASESOLGEN=$BINDIR/sha1_freestart76_basesolgen
+export GPUATTACK=$BINDIR/sha1_freestart76_gpuattack
+
 echo "Generating basesolutions for 21 minutes..."
 # Generate base solutions for 21 minutes
 SEED=$RANDOM$RANDOM$RANDOM
-bin/sha1_freestart76_basesolgen --seed 1$SEED -f -o fs76_1.bin > fs76_1.log &
-bin/sha1_freestart76_basesolgen --seed 2$SEED -f -o fs76_2.bin > fs76_2.log &
-bin/sha1_freestart76_basesolgen --seed 3$SEED -f -o fs76_3.bin > fs76_3.log &
-bin/sha1_freestart76_basesolgen --seed 4$SEED -f -o fs76_4.bin > fs76_4.log &
+$BASESOLGEN --seed 1$SEED -f -o basesol76_1.bin > basesol76_1.log &
+$BASESOLGEN --seed 2$SEED -f -o basesol76_2.bin > basesol76_2.log &
+$BASESOLGEN --seed 3$SEED -f -o basesol76_3.bin > basesol76_3.log &
+$BASESOLGEN --seed 4$SEED -f -o basesol76_4.bin > basesol76_4.log &
 
 echo "21 minutes left..."
 for ((i=20; i>=0; --i)); do 
@@ -20,37 +24,37 @@ killall sha1_freestart76_basesolgen
 for ((i=1;;++i)); do
 	# combine generated basesolutions in 1 file
 	inputs=""
-	for f in fs76_*.bin ; do
+	for f in basesol76_*.bin ; do
 		inputs="$inputs -i $f"
 	done
-	echo bin/sha1_freestart76_basesolgen -f -o fs76.bin $inputs
-	bin/sha1_freestart76_basesolgen -f -o fs76.bin $inputs
-	rm fs76_*.bin
+	echo $BASESOLGEN -f -o basesol76.run$i.bin $inputs
+	$BASESOLGEN -f -o basesol76.run$i.bin $inputs
+	rm basesol76_*.bin
 
 	# start new generation of basesolutions in background
 	SEED=$RANDOM$RANDOM$RANDOM
-	bin/sha1_freestart76_basesolgen --seed 1${i}0$SEED -f -o fs76_1.bin >> fs76_1.log &
-	bin/sha1_freestart76_basesolgen --seed 2${i}0$SEED -f -o fs76_2.bin >> fs76_2.log &
-	bin/sha1_freestart76_basesolgen --seed 3${i}0$SEED -f -o fs76_3.bin >> fs76_3.log &
-	bin/sha1_freestart76_basesolgen --seed 4${i}0$SEED -f -o fs76_4.bin >> fs76_4.log &
+	$BASESOLGEN --seed 1${i}0$SEED -f -o basesol76_1.bin >> basesol76_1.log &
+	$BASESOLGEN --seed 2${i}0$SEED -f -o basesol76_2.bin >> basesol76_2.log &
+	$BASESOLGEN --seed 3${i}0$SEED -f -o basesol76_3.bin >> basesol76_3.log &
+	$BASESOLGEN --seed 4${i}0$SEED -f -o basesol76_4.bin >> basesol76_4.log &
 
 	# start timer for 21 minutes
-	rm -f fs76.20min.timer
-	(sleep $((21*60)); touch fs76.20min.timer) &
+	rm -f basesol76.20min.timer
+	(sleep $((21*60)); touch basesol76.20min.timer) &
 
 	# start GPU attack
-	bin/sha1_freestart76_gpuattack -a -i fs76.bin -o fs76_q56_$i.bin | tee -a bs76_gpu.log
+	$GPUATTACK -a -i basesol76.run$i.bin -o fs76_q56_$i.bin | tee -a fs76_gpu.log
 	
 	# check for freestart collision
-	bin/sha1_freestart76_basesolgen -v -i fs76_q56_$i.bin > fs76_q56_$i.log
+	$BASESOLGEN -v -i fs76_q56_$i.bin > fs76_q56_$i.log
 	if grep "Found solution" -A52 -B80 fs76_q56_$i.log ; then
 		killall sha1_freestart76_basesolgen
 		exit 0
 	fi
 	
 	# wait till timer has elapsed if it has not already
-	while [ ! -f fs76.20min.timer ]; do sleep 10; done
-	rm -f fs76.20min.timer
+	while [ ! -f basesol76.20min.timer ]; do sleep 10; done
+	rm -f basesol76.20min.timer
 	
 	# kill background generation of basesolutions
 	killall sha1_freestart76_basesolgen
