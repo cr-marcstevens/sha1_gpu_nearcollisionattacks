71cec301d5b0a4da1da9041c922f99c9ba011383
cr-marcstevens <github@marc-stevens.nl>
2017-11-23 20:59:40 +0100

    Improved script

--- run_freestart76.sh
+++ run_freestart76.sh
@@ -4,31 +4,35 @@ export BINDIR=$(dirname $0)/bin
 export BASESOLGEN=$BINDIR/sha1_freestart76_basesolgen
 export GPUATTACK=$BINDIR/sha1_freestart76_gpuattack
 
-echo "Generating basesolutions for 21 minutes..."
-# Generate base solutions for 21 minutes
+echo "Generating basesolutions for 4 minutes..."
+# Generate base solutions for 4 minutes
 SEED=$RANDOM$RANDOM$RANDOM$RANDOM
 $BASESOLGEN --seed 1_$SEED -g -m 262144 -o basesol76_1.bin > basesol76_1.log &
 $BASESOLGEN --seed 2_$SEED -g -m 262144 -o basesol76_2.bin > basesol76_2.log &
 $BASESOLGEN --seed 3_$SEED -g -m 262144 -o basesol76_3.bin > basesol76_3.log &
 $BASESOLGEN --seed 4_$SEED -g -m 262144 -o basesol76_4.bin > basesol76_4.log &
 
-echo "21 minutes left..."
-for ((i=20; i>=0; --i)); do 
+echo "4 minutes left..."
+for ((i=4; i>=0; --i)); do 
 	sleep 60
 	echo "$i minutes left..."
 done
 
+sleep 5
 killall sha1_freestart76_basesolgen
 
 # Loop: run GPU attack on available basesolutions, meanwhile generate new basesolutions
 for ((i=1;;++i)); do
 	# combine generated basesolutions in 1 file
+	sleep 2
 	inputs=""
 	for f in basesol76_*.bin ; do
 		inputs="$inputs -i $f"
 	done
-	echo $BASESOLGEN -f -o basesol76.run$i.bin $inputs
-	$BASESOLGEN -f -o basesol76.run$i.bin $inputs
+	if [ -n "$inputs" ]; then
+		echo $BASESOLGEN -g -o basesol76.run$i.bin $inputs
+		$BASESOLGEN -g -o basesol76.run$i.bin $inputs
+	fi
 	rm basesol76_*.bin
 
 	# start new generation of basesolutions in background
